8141e0dbea0b7a22bdbd569b37006ba74f4f212d
paul <mannion.paul@gmail.com>
2013-12-21 17:57:55 +0000

    rspec and jquery slider added to search

--- app/models/giftsearch.rb
+++ app/models/giftsearch.rb
@@ -1,8 +1,12 @@
 class Giftsearch < ActiveRecord::Base
-  attr_accessible :min_spend, :max_spend, :category, :console_id, :show_games
+  attr_accessible :min_spend, :max_spend, :category, :console_id, :show_games, :amount
 
    belongs_to :supplier
    belongs_to :console
    belongs_to :user
 
+
+  def amount
+
+  end
 end
